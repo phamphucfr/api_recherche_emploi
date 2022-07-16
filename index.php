1adc67fef53b0e889927b6fbd51b8e121733dc75
Phuc-PHAM-NGOC <phuc.phamng@gmail.com>
2022-07-16 22:48:21 +0200

    Créer le dossier tools

--- index.php
+++ index.php
@@ -5,17 +5,14 @@ function classes_autoloader($class) {
     require_once 'classes/'.$class.'.php';
 }
 
-$cnx = null;
-
 switch($_SERVER['REQUEST_METHOD'])
 {
-case 'GET': echo("Mon nom est ".$_GET['name']." . J'ai ".$_GET['age']." ans.");
+case 'GET': if(count($_GET) === 0) echo("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
+            else echo("Mon nom est ".$_GET['name']." . J'ai ".$_GET['age']." ans.");
             break;
 case 'POST': echo($_POST); 
             break;
-default: $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-            echo($actual_link);
-            break;
+default:    break;
 }
 
 
